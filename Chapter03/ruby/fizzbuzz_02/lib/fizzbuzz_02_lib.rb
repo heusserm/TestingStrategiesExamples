52b389ae692d2399ec907cc36c8ed848a724a87b
heusserm <matt.heusser@gmail.com>
2022-12-13 14:26:20 -0500

    refactored to add proper plumbing but lack implementation

--- Chapter03/ruby/fizzbuzz_02/lib/fizzbuzz_02_lib.rb
+++ Chapter03/ruby/fizzbuzz_02/lib/fizzbuzz_02_lib.rb
@@ -6,13 +6,21 @@ class Fizzbuzz_02
 
   end
 
-
   def get_total_result(loop_max)
-    return "1\n"
+    result = "";
+    for count in 1..loop_max do
+      result = result + calc_individual_buzz_result(count);
+    end  
   end
 
   def calc_individual_buzz_result(input)
-    return "1\n"
+    if input % 3 == 0
+      return "Fizz"
+    elsif input % 5 == 0
+      return "Buzz"
+    else
+      return input.to_s()
+    end
   end
 
   #def get_total_result(loop_max)
