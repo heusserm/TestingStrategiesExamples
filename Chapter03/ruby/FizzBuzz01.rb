8a1d28b09cb69a3bb2be02cba76575632bbde314
heusserm <matt.heusser@gmail.com>
2022-12-13 11:51:50 -0500

    completed first draft

---
+++ Chapter03/ruby/FizzBuzz01.rb
@@ -0,0 +1,23 @@
+
+puts "FizzBuzz01.rb By Matthew Heusser for the Testing Stratgies book"
+puts "---------------------------------------------------------------"
+puts "\n"
+count_to = ARGV[0].to_i;
+
+if !defined?(count_to) or count_to<1
+  puts "Use: FizzBuzz01.rb (count)\n"
+  puts "Where count is a round number of value one or higher"
+  abort("");
+end
+
+for count in 1..count_to do
+  if count % 3 == 0 
+    puts "Fizz"
+  elsif count % 5 == 0
+    puts "Buzz"
+  else
+    puts count.to_s(); 
+  end
+end
+
+
